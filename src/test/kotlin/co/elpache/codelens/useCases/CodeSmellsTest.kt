eb12f8802bb4c0b374725e8637aa9d19acdf5bdc
ricardodps <ricardo.pacheco@thoughtworks.com>
2019-11-18 00:17:49 +0100

    Fix conflict

--- src/test/kotlin/co/elpache/codelens/useCases/CodeSmellsTest.kt
+++ src/test/kotlin/co/elpache/codelens/useCases/CodeSmellsTest.kt
@@ -1,53 +1,53 @@
 package co.elpache.codelens.useCases;
 
 import co.elpache.codelens.codeTree
-import co.elpache.codelens.codeTreeNode
 import co.elpache.codelens.createCodeSmellsUseCases
+import co.elpache.codelens.tree.vDataOf
 import org.assertj.core.api.Assertions
 import org.junit.Test;
 
 public class CodeSmellsTest {
 
-    @Test
-    fun `can get code smell list`() {
-        val smellPresets = CodeSmellsUseCases.getSmellPresets()
+  @Test
+  fun `can get code smell list`() {
+    val smellPresets = CodeSmellsUseCases.getSmellPresets()
 
-        Assertions.assertThat(smellPresets).isNotNull
-    }
+    Assertions.assertThat(smellPresets).isNotNull
+  }
 
-    @Test
-    fun `can get code smell by name`() {
-        val smellPreset = CodeSmellsUseCases.findSmellByName("longParameterList")
+  @Test
+  fun `can get code smell by name`() {
+    val smellPreset = CodeSmellsUseCases.findSmellByName("longParameterList")
 
-        Assertions.assertThat(smellPreset).isNotNull
-        Assertions.assertThat(smellPreset?.param).isEqualTo("params")
-    }
+    Assertions.assertThat(smellPreset).isNotNull
+    Assertions.assertThat(smellPreset?.param).isEqualTo("params")
+  }
 
-    @Test
-    fun `can get long parameter list functions`() {
-        val uc = createCodeSmellsUseCases(
-            codeTree(
-                "1",
-                codeTreeNode("type" to "file"),
-                codeTree(
-                    "2",
-                    codeTreeNode("type" to "files"),
-                    codeTree("3", codeTreeNode("type" to "fun", "params" to 2)),
-                    codeTree("4", codeTreeNode("type" to "fun", "params" to 2)),
-                    codeTree("5", codeTreeNode("type" to "fun", "params" to 4))
-                ),
-                codeTree("6", codeTreeNode("type" to "fun", "params" to 4)),
-                codeTree("7", codeTreeNode("type" to "fun", "params" to 2)),
-                codeTree("8", codeTreeNode("type" to "fun", "params" to 3))
-            )
-        )
+  @Test
+  fun `can get long parameter list functions`() {
+    val uc = createCodeSmellsUseCases(
+      codeTree(
+        "1",
+        vDataOf("type" to "file"),
+        codeTree(
+          "2",
+          vDataOf("type" to "files"),
+          codeTree("3", vDataOf("type" to "fun", "params" to 2)),
+          codeTree("4", vDataOf("type" to "fun", "params" to 2)),
+          codeTree("5", vDataOf("type" to "fun", "params" to 4))
+        ),
+        codeTree("6", vDataOf("type" to "fun", "params" to 4)),
+        codeTree("7", vDataOf("type" to "fun", "params" to 2)),
+        codeTree("8", vDataOf("type" to "fun", "params" to 3))
+      )
+    )
 
-        val results = uc.checkLongParameterList()
+    val results = uc.checkLongParameterList()
 
-        Assertions.assertThat(results.checkSmell).isTrue()
-        Assertions.assertThat(results.analyticsResults.rows).containsExactly(
-            listOf(4, 2)
-        )
-        //Assertions.assertThat(results.smellScore).isEqualTo(0.66)
-    }
+    Assertions.assertThat(results.checkSmell).isTrue()
+    Assertions.assertThat(results.analyticsResults.rows).containsExactly(
+      listOf(4, 2)
+    )
+    //Assertions.assertThat(results.smellScore).isEqualTo(0.66)
+  }
 }
